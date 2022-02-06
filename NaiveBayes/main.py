af8a9a2d1483ba0df8768072f14812fd459607a8
Eduardo Jimenez <eduardojimfan@gmail.com>
2022-02-06 02:03:38 -0500

    Calculate Accuracy and Precision of the Naive Bayes model

--- NaiveBayes/main.py
+++ NaiveBayes/main.py
@@ -15,3 +15,15 @@ count_vector = CountVectorizer(stop_words='english')
 
 training_data = count_vector.fit_transform(X_train)
 testing_data = count_vector.transform(X_test)
+
+from sklearn.naive_bayes import MultinomialNB
+naive_bayes = MultinomialNB()
+naive_bayes.fit(training_data, y_train)
+
+predictions = naive_bayes.predict(testing_data)
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+print('Accuracy score: ', format(accuracy_score(y_test, predictions)))
+print('Precision score: ', format(precision_score(y_test, predictions)))
+print('Recall score: ', format(recall_score(y_test, predictions)))
+print('F1 score: ', format(f1_score(y_test, predictions)))
